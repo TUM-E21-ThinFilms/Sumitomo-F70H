88cbd793ac5fc297711c0e56121285dd54cbaeeb
Alexander Book <alexander.book@frm2.tum.de>
2017-06-14 11:56:21 +0200

    adding ports library

--- sumitomo_f70h/factory.py
+++ sumitomo_f70h/factory.py
@@ -17,14 +17,18 @@ from protocol import SumitomoF70HProtocol
 from driver import SumitomoF70HDriver
 from e21_util.transport import Serial
 from e21_util.log import get_sputter_logger
+from e21_util.ports import Ports
 
 class SumitomoF70HFactory:
 	def get_logger(self):
 		return get_sputter_logger('Sumitomo F-70H', 'sumitomo.log')
 	
-	def create(self, device='/dev/ttyUSB13', logger=None):
+	def create(self, device=None, logger=None):
 		if logger is None:
 			logger = self.get_logger()
+
+		if device is None:
+			device = Ports().get_port(Ports.DEVICE_COMPRESSOR)
 
 		protocol = SumitomoF70HProtocol(logger=logger)
 		return SumitomoF70HDriver(Serial(device, 9600, 8, 'N', 1, 1), protocol)
